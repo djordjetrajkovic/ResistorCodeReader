f6a3b609e823230c42186ea22d1c1bb4240fd8b4
djordjetrajkovic <djordje.trajkovic@gmail.com>
2020-04-14 18:34:37 +0200

    segmentation code added

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+#include "segmentation.h"
+
 int main()
 {
+    string slika = "/home/djordje/Pictures/test_noise.png";
+    string pozadina = "/home/djordje/Pictures/light.png";
+    segment::funct(slika, pozadina);
     cout << "OK!" << endl;
     return 0;
 }
---
+++ library/include/segmentation.h
@@ -0,0 +1,4 @@
+namespace segment
+{
+    int funct(string, string);
+}
