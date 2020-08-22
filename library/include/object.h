8594b2104d9ea731b304b5bb2bc866460e33a8ff
djordjetrajkovic <djordje.trajkovic@gmail.com>
2020-08-22 19:56:12 +0200

    preradjene klase Object....

--- library/include/object.h
+++ library/include/object.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <utility>
 using namespace std;
 
 // OpenCV includes
@@ -11,18 +12,40 @@ using namespace cv;
 namespace objectsnmsp
 {
     class Object {
+    private:
+        void copy(const Object&);
+        void mov(Object&);
+        void del();
     protected:
         Mat* image;
+        Mat imagepatterngrayscale;
+        Mat imagepatternbinary;
         Rect roi;
         vector<Point> contour;
         RotatedRect rotrectangle;
-        Rect rectangle;
         string name;
         string category;
         string type;
     public:
-        Object() {}
+        // konstruktori
+        Object() = default;
+        Object(const Object& obj)
+        {
+            copy(obj);
+        }
+        Object(Object&& obj)
+        {
+            mov(obj);
+        }
 
+        // polimorfno kopiranje objekata
+        virtual Object* clone() const& = 0;
+        virtual Object* clone() && = 0;
+
+        // destruktor
+        virtual ~Object() = 0;
+
+        // metode
         void setImage(Mat* img)
         {
             image = img;
@@ -30,6 +53,26 @@ namespace objectsnmsp
         Mat* getImage()
         {
             return image;
+        }
+        
+        Mat getImagePatternGrayscale()
+        {
+            return imagepatterngrayscale;
+        }
+
+        void setImagePatternGrayscale(Mat image)
+        {
+            imagepatterngrayscale = image;
+        }
+
+        Mat getImagePatternBinary()
+        {
+            return imagepatternbinary;
+        }
+
+        void setImagePatternBinary(Mat image)
+        {
+            imagepatternbinary = image;
         }
 
         void setRoi(Rect roi)
@@ -59,25 +102,26 @@ namespace objectsnmsp
             rotrectangle = rrect;
         }
 
-        Rect getRect()
-        {
-            return rectangle;
-        }
-        void setRect(Rect rect)
-        {
-            rectangle = rect;
-        }
-
         // virtual methods
         virtual const string getCategory() const = 0;
         virtual const string getType() const = 0;
         virtual string getDescription() = 0;
-
     };
 
     class Unknown: public Object 
     {
         public:
+        Unknown() = default;
+        Unknown(const Unknown& unknwn): Object(unknwn){}
+        Unknown(Unknown&& unknwn): Object(unknwn){}
+        Unknown* clone() const& override
+        {
+            return new Unknown(*this);
+        }
+        Unknown* clone() && override
+        {
+            return new Unknown(move(*this));
+        }
         const string getCategory() const override { return "Unknown"; }
         const string getType() const override { return "Unknown"; }
         string getDescription() override { return "Unknown"; }
@@ -86,7 +130,12 @@ namespace objectsnmsp
     class Electronics: public Object
     {
         public:
-        Electronics(){}
+        Electronics() = default;
+        Electronics(const Electronics& electronics): Object(electronics){}
+        Electronics(Electronics&& electronics): Object(electronics){}
+
+        virtual Electronics* clone() const& = 0;
+        virtual Electronics* clone() && = 0;
         const string getCategory() const override { return "Electronics"; }
         virtual const string getType() const = 0;
         virtual string getDescription() = 0;        
@@ -96,13 +145,28 @@ namespace objectsnmsp
     {
         private:
         Scalar resistorColor;
-        Scalar ringone;
-        Scalar ringtwo;
-        Scalar ringthree;
-        Scalar ringfour;
-        Scalar ringfive;
+        Scalar rone;
+        Scalar rtwo;
+        Scalar rthree;
+        Scalar rfour;
+        Scalar rfive;
+        void copy(const Resistor&);
+        void mov(Resistor&);
+
         public:
-        Resistor(){}
+        Resistor() = default;
+        Resistor (const Resistor& resistor);
+        Resistor (Resistor&& resistor);
+        
+        Resistor* clone() const& override
+        {
+            return new Resistor(*this);
+        }
+        Resistor* clone() && override
+        {
+            return new Resistor(move(*this));
+        }
+
         const string getType() const override { return "Resistor"; }
         string getDescription() override;
     };
