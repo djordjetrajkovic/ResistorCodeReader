089643289b199594e8c52153482a40ef394807dd
djordjetrajkovic <djordje.trajkovic@gmail.com>
2020-08-22 19:56:12 +0200

    preradjene klase Object....

--- library/src/objects.cpp
+++ library/src/objects.cpp
@@ -9,7 +9,73 @@ using namespace cv;
 
 #include "object.h"
 
+objectsnmsp::Object::~Object()
+{
+    del();
+}
+
+void objectsnmsp::Object::copy(const Object& obj)
+{
+    image = obj.image;
+    roi = obj.roi;
+    contour = obj.contour;
+    rotrectangle = obj.rotrectangle;
+    rectangle = obj.rectangle;
+    name = obj.name;
+    category = obj.category;
+    type = obj.type;
+}
+
+void objectsnmsp::Object::mov(Object& obj)
+{
+    image = obj.image;
+    roi = obj.roi;
+    contour = obj.contour;
+    rotrectangle = obj.rotrectangle;
+    rectangle = obj.rectangle;
+    name = obj.name;
+    category = obj.category;
+    type = obj.type;
+}
+
+void objectsnmsp::Object::del()
+{
+    delete image;
+    for (auto &cont: contour) delete &cont;
+    contour.clear();
+}
+
 string objectsnmsp::Resistor::getDescription()
 {
     return "resistor value by colors";
+}
+
+objectsnmsp::Resistor::Resistor(const Resistor& resistor):Electronics(resistor)
+{
+    copy(resistor);
+}
+
+objectsnmsp::Resistor::Resistor(Resistor&& resistor):Electronics(resistor)
+{
+    mov(resistor);
+}
+
+void objectsnmsp::Resistor::copy(const Resistor& resistor)
+{
+    resistorColor = resistor.resistorColor;
+    rone = resistor.rone;
+    rtwo = resistor.rtwo;
+    rthree = resistor.rthree;
+    rfour = resistor.rfour;
+    rfive = resistor.rfive;
+}
+
+void objectsnmsp::Resistor::mov(Resistor& resistor)
+{
+    resistorColor = resistor.resistorColor;
+    rone = resistor.rone;
+    rtwo = resistor.rtwo;
+    rthree = resistor.rthree;
+    rfour = resistor.rfour;
+    rfive = resistor.rfive;
 }
